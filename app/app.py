71e1b5c2a74cd2593ab01e07c0de282d5ce8fc3f
Alan Sanchez <alan.sanchez@alumnos.udg.mx>
2016-12-01 14:48:52 -0600

    Se insertan dos roles y un usuario en cada rol.

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, \
                     TextAreaField
 from wtforms.validators import Required, Length, DataRequired
 # from flask_mongoengine import MongoEngine
-from flask_security import current_user, login_user
+from flask_security import current_user, login_user, utils
 from flask_security.core import UserMixin, AnonymousUser
 import config
 from db import user_datastore
@@ -22,9 +22,30 @@ db_sql = config.db_sql
 def create_user():
     db_sql.drop_all()
     db_sql.create_all()
-    user_datastore.create_user(email='alan', password='password')
-    user_datastore.commit()
+    # Create the Roles "admin" and "end-user" -- unless they already exist
+    user_datastore.find_or_create_role(name='admin', description='Administrator')
+    user_datastore.find_or_create_role(name='end-user', description='End user')
 
+    # Create two Users for testing purposes -- unless they already exists.
+    # In each case, use Flask-Security utility function to encrypt the password.
+    encrypted_password = utils.encrypt_password('password')
+    if not user_datastore.get_user('alan'):
+        user_datastore.create_user(email='alan', password=encrypted_password)
+    if not user_datastore.get_user('admin'):
+        user_datastore.create_user(email='admin', password=encrypted_password)
+
+    # Commit any database changes; the User and Roles must exist before we can add a Role to the User
+    db_sql.session.commit()
+
+    # Give one User has the "end-user" role, while the other has the "admin" role. (This will have no effect if the
+    # Users already have these Roles.) Again, commit any database changes.
+    user_datastore.add_role_to_user('alan', 'end-user')
+    user_datastore.add_role_to_user('admin', 'admin')
+    db_sql.session.commit()
+
+    #
+    # https://gist.github.com/skyuplam/ffb1b5f12d7ad787f6e4
+    #
 
 class user_role_form(FlaskForm):
     user = StringField(u'Usuario', validators=[DataRequired])
