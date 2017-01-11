f69db22032ea1850b0d19b2c0db9128de95b0eb2
Alan <pony_alanandres@hotmail.com>
2017-01-11 12:56:34 -0600

    Se modificó al módulo para que se puedan importar las clases más fácilmente.

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from Computadora import Computadora
+from Lugar import Lugar
+from Ocupacion import Ocupacion
+from Registro import Registro
+from Rol import Rol
+from TipoRegistro import TipoRegistro
+from Usuario import Usuario
