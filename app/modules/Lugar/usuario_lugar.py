9f136cb70d9469aa10ed33b809bb90821b9cb949
Alan Sánchez <alan.sanchez@alumnos.udg.mx>
2017-03-08 13:41:16 -0600

    Se comienza a generar el qr dinámico.

--- app/modules/Lugar/usuario_lugar.py
+++ app/modules/Lugar/usuario_lugar.py
@@ -1,19 +1,40 @@
 # -*- coding: utf-8 -*-
 from app.config import db_sql
-from flask import render_template
+from flask import render_template, flash
 # from datetime import datetime
 # from app.utils import key_utils
 from flask_login import current_user
-from app.models import Lugares_Usuarios
+from app.models import Lugares_Usuarios, Lugar
+from CheckLugar import CheckLugar
 
 
-class UsuarioLugar(object):
-    def __init__(self, usuario, lugar):
-        self.usuario = usuario
-        self.lugar = lugar
+class CheckLugarDinamico(CheckLugar):
+    def __init__(self, id_lugar, usuario, key_dinamico):
+        self.key_dinamico = key_dinamico
+        self.set_usuario(usuario)
+        CheckLugar.__init__(self, id_lugar=id_lugar)
+        self.obten_lugar()
 
-    def generate_token(self, lugar):
-        pass
+    def obten_lugar(self):
+        """
+        Si existe un Lugar con ese id y esa llave dinámica, entonces guarda en
+        self.lugar el objeto Lugar y lo retorna. Si no existe el lugar entonces
+        guardará y retornará None.
+        :return: app.models.Lugar
+        """
+        lugar_usuario = db_sql.session.query(Lugares_Usuarios).filter(
+            Lugares_Usuarios.id_lugar == self.id_lugar,
+            Lugares_Usuarios.id_usuario == self.usuario.id,
+            Lugares_Usuarios.token == self.key_dinamico
+        )
+        if lugar_usuario.count() > 0:
+            query = db_sql.session.query(Lugar.id, Lugar.nombre).filter(
+                (Lugar.id == self.id_lugar)
+            )
+            self.lugar = query.first()
+        else:
+            self.lugar = None
+        return self.lugar
 
 
 def generar_qr_lugar(id):
@@ -22,5 +43,11 @@ def generar_qr_lugar(id):
             Lugares_Usuarios.id_lugar == id,
             Lugares_Usuarios.id_usuario == current_user.id
         )
-        # print(lugar_usuario.first())
-        return render_template('index.html')
+        if(lugar_usuario.first()):
+            flash(u'Sí tienes permisos para generar QR de este lugar',
+                  category='warning')
+            return render_template('index.html')
+        else:
+            flash(u'No tienes permisos para generar QR de este lugar',
+                  category='warning')
+            return render_template('index.html')
