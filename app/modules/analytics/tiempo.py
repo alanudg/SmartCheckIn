997d7f4db0def0824efc487797b9d2952bb4c433
Bearnal19 <cesar_enigma19@hotmail.com>
2017-03-15 05:32:21 -0600

    Modificacion de analytics

--- app/modules/analytics/tiempo.py
+++ app/modules/analytics/tiempo.py
@@ -1,9 +1,13 @@
 from wtforms import DateField, validators
-from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
+from flask import request, flash, render_template, url_for, redirect, Markup
 from flask_login import current_user
 
 
 class intervalo_tiempo_form(FlaskForm):
     fecha_entrada = DateField('Start Date', format='%m/%d/%Y', validators=(
         validators.Optional(),))
+
+def entradas():
+    fecha = request.args.get('fecha_hora_entrada')
+    formulario = intervalo_tiempo_form(csrf_enabled=False)
