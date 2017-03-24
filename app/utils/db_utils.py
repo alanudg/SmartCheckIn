32b913609bbffc8682cb288e7931f9d3cc217ed9
Alan Sánchez <alan.sanchez@alumnos.udg.mx>
2017-03-24 14:31:06 -0600

    Se agregaron más casos de detalles_recursos, así como un nuevo usuario de ejemplo.

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -77,6 +77,17 @@ def create_sample_db(db_sql, user_datastore):
                                    nombres='Alan Andrés',
                                    lugares=[l_cici]
                                    )
+    if not user_datastore.get_user('andres'):
+        user_datastore.create_user(email='andres',
+                                   password=encrypted_password,
+                                   id_ocupacion=ocup_alumno.id,
+                                   codigo='1234',
+                                   nip='1111',
+                                   apellido_paterno='Sánchez',
+                                   apellido_materno='Castro',
+                                   nombres='Alan Andrés',
+                                   lugares=[l_cici]
+                                   )
     if not user_datastore.get_user('admin'):
         user_datastore.create_user(email='admin',
                                    password=encrypted_password)
@@ -89,11 +100,13 @@ def create_sample_db(db_sql, user_datastore):
     # has the "admin" role. (This will have no effect if the
     # Users already have these Roles.) Again, commit any database changes.
     user_datastore.add_role_to_user('alan', 'end-user')
+    user_datastore.add_role_to_user('andres', 'end-user')
     user_datastore.add_role_to_user('admin', 'admin')
     user_datastore.add_role_to_user('admin', 'end-user')
     db_sql.session.commit()
 
     alan = user_datastore.get_user('alan')
+    andres = user_datastore.get_user('andres')
 
     entrada1 = Registro(id_usuario=alan.id,
                         id_lugar=l_cici.id,
@@ -117,7 +130,7 @@ def create_sample_db(db_sql, user_datastore):
                     id_registro_entrada=entrada1.id,
                     id_registro_salida=entrada1.id,
                     fecha_hora_toma=datetime.utcnow() - timedelta(minutes=5),
-                    fecha_hora_entrega=datetime.utcnow() - timedelta(minutes=4)
+
                     )
     db_sql.session.add(toma2)
     db_sql.session.commit()
@@ -125,21 +138,31 @@ def create_sample_db(db_sql, user_datastore):
     entrada1.fecha_hora_salida = datetime.utcnow() - timedelta(minutes=2)
     db_sql.session.commit()
 
-    entrada2 = Registro(id_usuario=alan.id,
+    entrada2 = Registro(id_usuario=andres.id,
                         id_lugar=l_cici.id,
                         fecha_hora_entrada=datetime.utcnow() -
-                        timedelta(minutes=70))
+                        timedelta(minutes=60))
     db_sql.session.add(entrada2)
     db_sql.session.commit()
 
+    entrada3 = Registro(id_usuario=alan.id,
+                        id_lugar=l_cici.id,
+                        fecha_hora_entrada=datetime.utcnow() -
+                        timedelta(minutes=70))
+    db_sql.session.add(entrada3)
+    db_sql.session.commit()
+
+    toma2.id_registro_salida = entrada3.id
+    toma2.fecha_hora_entrega = datetime.utcnow() - timedelta(minutes=4)
+
     toma3 = Detalle_registro(
                     id_computadora=computadora1.id,
-                    id_registro_entrada=entrada2.id,
-                    id_registro_salida=entrada2.id,
+                    id_registro_entrada=entrada3.id,
+                    id_registro_salida=entrada3.id,
                     fecha_hora_toma=datetime.utcnow() - timedelta(minutes=65),
                     )
     db_sql.session.add(toma3)
     db_sql.session.commit()
 
-    entrada2.fecha_hora_salida = datetime.utcnow() - timedelta(minutes=60)
+    entrada3.fecha_hora_salida = datetime.utcnow() - timedelta(minutes=60)
     db_sql.session.commit()
