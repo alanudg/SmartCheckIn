f5dd4c63f7575b28ca552e9ca17f67072418e1f6
Alan Sánchez <alan.sanchez@alumnos.udg.mx>
2017-04-05 14:22:29 -0500

    Se documentó el módulo MQTT

--- app/modules/MQTT/__init__.py
+++ app/modules/MQTT/__init__.py
@@ -12,6 +12,13 @@ db_sql = config.db_sql
 
 
 def generate_auth_token(expiration=600):
+        """
+        Genera un token que será válido por los segundos definidos en
+        expiration
+        :param expiration: Segundos que durará el token antes de ser inválido
+        :type expiration: int
+        :return: string
+        """
         s = Serializer(config.app.config['SECRET_KEY'], expires_in=expiration)
         # TODO autenticar con credenciales específicas
         # de la puerta/lugar
@@ -19,6 +26,12 @@ def generate_auth_token(expiration=600):
 
 
 def verify_auth_token(token):
+        """
+        Verifica si un token es válido o no
+        :param token: El token a verificar
+        :type token: string
+        :return: object
+        """
         s = Serializer(config.app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
@@ -42,11 +55,23 @@ def verify_password(username_or_token):
 
 
 class MQTT:
+    """
+    Clase que se encarga de verificar los canales de comunicación con los
+    sensores de las puertas
+    :param host: IP o nombre del dominio donde se encuentra el servidor MQTT
+    :type id_lugar: string
+    :param port: Puerto en donde se encuentra corriendo el servidor MQTT
+    :type port: int
+    :param sub_channel: Canal al que se suscribirá por defecto el cliente MQTT
+    :type sub_channel: string
+    :param pub_prefix: Prefijo del canal al que pubicará el cliente MQTT
+    :type pub_prefix: string
+    """
     def __init__(self,
                  host='localhost',
                  port=1883,
                  sub_channel='lugares/query/#',
-                 pub_prefix='lugares/acceso/'):
+                 pub_prefix='boards/instructions/'):
         try:
             self.host = str(host)
             self.port = int(port)
@@ -62,27 +87,62 @@ class MQTT:
         self.client.loop_start()
 
     def check(self, usuario, lugar, msg):
-        check = CheckLugar(lugar.id, lugar.key)
-        check.set_usuario(usuario.first())
-        e, message = check.valida_entrada_salida_lugar()
-        if(e):
-            publish.single(self.pub_prefix +
-                           msg.topic.split('/')[-2] +
-                           '/' + msg.topic.split('/')[-1], json.dumps({
-                                'instruction': 'OPEN',
-                                'secure-key': 1234,
-                            }), hostname=self.host)
+        """
+        Verifica que un usuario pueda aplicar un evento en cierto lugar.
+        Si no existe el usuario publicará un mensaje con la instrucción
+        USER_DOESNT_EXIST.
+        Si el usuario no tiene acceso, publicará un mensaje con la instrucción
+        ACCESS_ERROR
+        Si el usuario pudo crear una entrada/salida en el lugar, publicará una
+        instrucción
+        OPEN
+        :param usuario: Usuario que intenta aplicar un evento
+        :type usuario: SQLAlchemy Query Object [Usuario]
+        :param usuario: Lugar en donde se intenta aplicar el evento
+        :type usuario: Lugar
+        :param msg: Mensaje recibido previamente por medio de MQTT
+        :type msg: {topic: String}
+        """
+        if(usuario.count()):
+            check = CheckLugar(lugar.id, lugar.key)
+            check.set_usuario(usuario.first())
+            e, message = check.valida_entrada_salida_lugar()
+            if(e):
+                publish.single(self.pub_prefix +
+                               msg.topic.split('/')[-2] +
+                               '/' + msg.topic.split('/')[-1], json.dumps({
+                                    'instruction': 'OPEN',
+                                    'secure-key': 1234,
+                                }), hostname=self.host)
+            else:
+                publish.single(self.pub_prefix +
+                               msg.topic.split('/')[-2] +
+                               '/' + msg.topic.split('/')[-1], json.dumps({
+                                    'instruction': 'ACCESS_ERROR',
+                                    'secure-key': 1234,
+                                    'message': str(message),
+                                }), hostname=self.host)
         else:
             publish.single(self.pub_prefix +
                            msg.topic.split('/')[-2] +
                            '/' + msg.topic.split('/')[-1], json.dumps({
-                                'instruction': 'ACCESS_ERROR',
+                                'instruction': 'USER_DOESNT_EXIST',
                                 'secure-key': 1234,
                                 'message': str(message),
                             }), hostname=self.host)
 
     def wrong_credentials(self, msg, json_data):
-        publish.single('boards/instructions/' +
+        """
+        Publica la instrucción WRONG_CREDENTIALS en el canal especificado en
+        self.pub_prefix + msg.topic.
+        Además de la instrucción, en el mensaje se incluye el lugar y la
+        puerta, así como un mensaje en query (indicado en json_data).
+        :param msg: Mensaje recibido previamente por medio de MQTT
+        :type msg: {topic: String}
+        :param json_data: Mensaje a enviar por medio del cliente MQTT
+        :type json_data: dict
+        """
+        publish.single(self.pub_prefix +
                        msg.topic.split('/')[-2] +
                        '/' + msg.topic.split('/')[-1], json.dumps({
                             'instruction': 'WRONG_CREDENTIALS',
@@ -91,21 +151,68 @@ class MQTT:
                             'door': msg.topic.split('/')[-1]
                         }), hostname=self.host)
 
-    def regenerate_token(self, msg):
-        publish.single('boards/instructions/' +
+    def wrong_args(self, msg, json_data):
+        """
+        Publica la instrucción WRONG_ARGS en el canal especificado en
+        self.pub_prefix + msg.topic.
+        Además de la instrucción, en el mensaje se incluye el lugar y la
+        puerta, así como un mensaje en query (indicado en json_data).
+        :param msg: Mensaje recibido previamente por medio de MQTT
+        :type msg: {topic: String}
+        :param json_data: Mensaje a enviar por medio del cliente MQTT
+        :type json_data: dict
+        """
+        publish.single(self.pub_prefix +
+                       msg.topic.split('/')[-2] +
+                       '/' + msg.topic.split('/')[-1], json.dumps({
+                            'instruction': 'WRONG_ARGS',
+                            'query': json.dumps(json_data),
+                            'place': msg.topic.split('/')[-2],
+                            'door': msg.topic.split('/')[-1]
+                        }), hostname=self.host)
+
+    def regenerate_token(self, msg, expires_in=20):
+        """
+        Publica la instrucción REGENERATE_TOKEN, acompañado el token que será
+        válido por los segundos definidos en expires_in
+        :param msg: Mensaje recibido previamente por medio de MQTT
+        :type msg: {topic: String}
+        :param expires_in: Tiempo en segundos que durará el token
+        :type expires_in: int
+        """
+        publish.single(self.pub_prefix +
                        msg.topic.split('/')[-2] +
                        '/' + msg.topic.split('/')[-1],
                        json.dumps({
                             'instruction': 'REGENERATE_TOKEN',
                             # TODO definir el tiempo de expiración del token en
                             # la configuración de la aplicación
-                            'token': generate_auth_token(20)
+                            'token': generate_auth_token(expires_in)
                         }), hostname=self.host)
 
     def on_connect(self, client, userdata, flags, rc):
+        """
+        Método a realizar cuando el cliente MQTT se conecte de manera correcta
+        con el servidor.
+        En este caso se suscribe al canal especificado en self.sub_channel
+        """
         self.client.subscribe(self.sub_channel, 0)
 
     def on_message(self, client, userdata, msg):
+        """
+        Método a realizar cuando el cliente MQTT reciba un mensaje en el canal
+        especificando en self.sub_channel
+
+        Se espera recibir un texto con formato json con la siguiente
+        información (se puede generar con json.dumps(dict)):
+        {
+            'tag': String (Tag del usuario que quiere aplicar algún evento),
+            'token': String (Opcional, si no se envía o es vacío, se intentará
+                             con 'user'/'password'),
+            'user': String (Opcional),
+            'password': String (Opcional)
+        }
+        """
         # mosquitto_sub -h localhost -t 'lugares/acceso/CUCEA/puerta1'
         # python pub-instruction.py
         # python pub-local.py -p CUCEA -d puerta1 -t GHIJKL -k 123456
@@ -116,29 +223,48 @@ class MQTT:
             print('Fallo')
 
         if(json_data is not None):
+            # Primero se obtiene el lugar con la información del canal a donde
+            # se publicó el mensaje
             nombre_lugar = msg.topic.split('/')[-2]
             lugar = db_sql.session.query(Lugar).filter(
                 Lugar.nombre == nombre_lugar
             )
+
+            # Después se obtiene al usuario por medio de su tag
             usuario = db_sql.session.query(Usuario).filter(
                 Usuario.token == str(json_data['tag'])
             )
             lugar = lugar.first()
+
             try:
+                # Si el mensaje recibido cuenta con un token, se intenta
+                # validar
                 token = json_data['token']
                 if(verify_password(token)):
+                    # Si el token es válido, se intenta aplicar el evento
                     self.check(usuario, lugar, msg)
                 else:
+                    # En caso contrario, se envía la instrucción
+                    # WRONG_CREDENTIALS
                     self.wrong_credentials(msg, json_data)
             except:
                 try:
+                    # En caso de que no exista la llave 'token' en el mensaje,
+                    # se intenta usar la combinación 'user'/'password'.
+
                     # TODO autenticar con credenciales específicas
                     # de la puerta/lugar
                     if(json_data['user'] == 'admin' and
                        json_data['password'] == 'admin'):
+                        # Si se reciben las credenciales correctas, se procede
+                        # a intentar realizar el evento en el lugar dado
                         self.check(usuario, lugar, msg)
+                        # Y se envía un token nuevo (Se enviarán dos mensajes)
                         self.regenerate_token(msg)
                     else:
+                        # Credenciales incorrectas, se envía WRONG_CREDENTIALS
                         self.wrong_credentials(msg, json_data)
                 except:
-                    self.wrong_credentials(msg, json_data)
+                    # En caso de que no existan esas llaves se envía la
+                    # instrucción WRONG_ARGS
+                    self.wrong_args(msg, json_data)
--- app/modules/MQTT/pub-instruction-example.py
+++ app/modules/MQTT/pub-instruction-example.py
@@ -8,6 +8,24 @@ import json
 
 
 class MQTT:
+    """
+    Clase que se encarga de verificar los canales de comunicación con los
+    sensores de las puertas
+    :param host: IP o nombre del dominio donde se encuentra el servidor MQTT
+    :type id_lugar: string
+    :param port: Puerto en donde se encuentra corriendo el servidor MQTT
+    :type port: int
+    :param sub_channel: Canal al que se suscribirá por defecto el cliente MQTT
+    :type sub_channel: string
+    :param pub_prefix: Prefijo del canal al que pubicará el cliente MQTT
+    :type pub_prefix: string
+    :param on_connect: Callback que se usará cuando el cliente se conecte con
+                       éxito al servidor MQTT
+    :type on_connect: Callback
+    :param on_message: Callback que se usará cuando el cliente detecte un
+                       mensaje enviado al canal self.sub_channel
+    :type on_connect: Callback
+    """
     def __init__(self,
                  host='localhost',
                  port=1883,
@@ -37,9 +55,28 @@ class MQTT:
         self.client.loop_forever()
 
     def on_connect(self, client, userdata, flags, rc):
+        """
+        Método a realizar cuando el cliente MQTT se conecte de manera correcta
+        con el servidor.
+        En este caso se suscribe al canal especificado en self.sub_channel
+        """
         self.client.subscribe(self.sub_channel, 0)
 
     def on_message(self, client, userdata, msg):
+        """
+        Método a realizar cuando el cliente MQTT reciba un mensaje en el canal
+        especificando en self.sub_channel
+
+        Se espera recibir un texto con formato json con la siguiente
+        información (se puede generar con json.dumps(dict)):
+        {
+            'tag': String (Tag del usuario que quiere aplicar algún evento),
+            'token': String (Opcional, si no se envía o es vacío, se intentará
+                             con 'user'/'password'),
+            'user': String (Opcional),
+            'password': String (Opcional)
+        }
+        """
         json_data = None
         try:
             json_data = json.loads(msg.payload)
@@ -48,6 +85,8 @@ class MQTT:
         place = msg.topic.split('/')[-2]
         door = msg.topic.split('/')[-1]
         if(json_data['instruction'] == 'ACCESS'):
+            # Intenta enviar un mensaje de acceso al servidor MQTT para que lo
+            # "escuche" el servidor web
             json_data['token'] = self.token
             print('Intentando con token en: lugares/query/' +
                   str(place) + '/' + str(door))
@@ -55,9 +94,14 @@ class MQTT:
                            json.dumps(json_data),
                            hostname="localhost")
         elif(json_data['instruction'] == 'REGENERATE_TOKEN'):
+            # Regenera el token de la puerta, sustituyéndolo por el que hubiera
+            # enviado el servidor web
             self.token = json_data['token']
             print('Actualiza token: ' + str(self.token))
         elif(json_data['instruction'] == 'WRONG_CREDENTIALS'):
+            # Credenciales incorrectas (cuando falla autentificación por token)
+            # Intenta hacer la petición utilizando las credenciales
+            # usuario/password
             query = json.loads(json_data['query'])
             query['user'] = 'admin'
             query['password'] = 'admin'
@@ -69,6 +113,11 @@ class MQTT:
                            str(json_data['door']) + '',
                            json.dumps(query),
                            hostname="localhost")
+        elif(json_data['instruction'] == 'OPEN'):
+            # Recibe un mensaje de apertura de puerta
+            # TODO
+            # Validar seguridad del mensaje
+            print("Abrir puerta")
 
 
 mq = MQTT(host='localhost',
